1fd21840c763fa4471ea1e6de1ce67be0f0f118a
Rahmat-786 <kingjeetmat@gmail.com>
2025-02-13 16:41:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -325,6 +325,6 @@ def analyze_image():
 @app.route('/rah')
 def fun():
     return ("<h1>hello world</h1>")
-
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(host='0.0.0.0', debug=True)
+
